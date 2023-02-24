cebcb7c915a7dd0e3e8252234d10a2718e721cd3
Jithin Johnson <jithu7432@gmail.com>
2023-02-24 22:15:21 +0530

    rust init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::fs;
+
+static image: &'static [i32][i32] = &[1, 2, 3, 4, 5];
+
+fn save_image_as_ppm() {}
+
+fn main() {
+    println!("Hello, world!");
+    println!("Hello, world!");
+    save_image_as_ppm()
+}
